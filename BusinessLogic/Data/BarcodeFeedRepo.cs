6e33dd0c6468dc6b13464553c79d3fd0e65b3dc7
Adrian Cadag <adrian_cadag@manulife.com>
2023-05-16 14:56:06 -0400

    Initial feature of barcode

--- BusinessLogic/Data/BarcodeFeedRepo.cs
+++ BusinessLogic/Data/BarcodeFeedRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,113 @@ namespace BusinessLogic.Data
                              pf.IsValid == true)
                 .ToList();
             return barcodeFeeds;
+        }
+
+        public bool AddBarcodeFeeds(
+            int projectId,
+            string dropoffPath,
+            string stagingPath,
+            string archivePath)
+        {
+            /* Check if folder(s) exist */
+            if (!Directory.Exists(dropoffPath))
+                throw new Exception(String.Format("Error : Dropoff {0} does not exist", dropoffPath));
+
+            if (!Directory.Exists(stagingPath))
+                throw new Exception(String.Format("Error : Staging {0} does not exist", stagingPath));
+
+            if (!Directory.Exists(archivePath))
+                throw new Exception(String.Format("Error : Archive {0} does not exist", archivePath));
+
+            /* Check if zero files */
+            int fileCount = Directory.GetFiles(dropoffPath).Length;
+            if (fileCount == 0)
+                throw new Exception(String.Format("No files on {0}", dropoffPath));
+
+            /* Get all text files */
+            string[] files = Directory.GetFiles(dropoffPath, "*.txt");
+            foreach (string file in files)
+            {
+                bool isValid = true;
+                string invalidFileMessage = String.Empty;
+
+                /* Check if file is empty - move to exceptions */
+                int rowCount = File.ReadLines(file).Count();
+                if (rowCount == 0)
+                {
+                    isValid = false;
+                    invalidFileMessage = "Empty feed";
+                }
+
+                /* Valid file if it reaches here */
+                string fileName = Path.GetFileName(file);
+
+                /* Check if file is either ANNUAL.TXT or ALL_RECORDS.TXT */
+                if (fileName.ToUpper() != "ANNUAL.TXT" &&
+                    fileName.ToUpper() != "ALL_RECORDS.TXT")
+                {
+                    isValid = false;
+                    invalidFileMessage = String.Format("Unexpected feed name : {0}", fileName);
+                }
+
+                BarcodeFeed forUpdate = _appDbContext.BarcodeFeeds
+                    .Where(upd => upd.FileName == fileName && upd.ProjectId == projectId).FirstOrDefault();
+                if (forUpdate != null)
+                {
+                    forUpdate.IsProcessed = false;
+                    forUpdate.RowCount = rowCount;
+                    forUpdate.IsValid = isValid;
+                    forUpdate.ExceptionReason =
+                        !String.IsNullOrEmpty(invalidFileMessage) ? invalidFileMessage : null;
+                }
+                else
+                {
+                    BarcodeFeed bf = new BarcodeFeed()
+                    {
+                        FileName = fileName,
+                        IsProcessed = false,
+                        ProjectId = projectId,
+                        RowCount = rowCount,
+                        IsValid = isValid,
+                        ExceptionReason = !String.IsNullOrEmpty(invalidFileMessage) ? invalidFileMessage : null
+                    };
+
+                    _appDbContext.BarcodeFeeds.Add(bf);
+                }
+
+                /* Copy the valid file to staging */
+                if (isValid)
+                {
+                    string stagingWFile;
+                    if (stagingPath.EndsWith("\\"))
+                        stagingWFile = stagingPath + fileName;
+                    else
+                        stagingWFile = stagingPath + "\\" + fileName;
+
+                    File.Copy(file, stagingWFile, true);
+                }
+            }
+
+            /* Move all to archive */
+            string archiveWDate;
+            if (archivePath.EndsWith("\\"))
+                archiveWDate = archivePath + DateTime.Now.ToString("yyyyMMdd-hhmmss");
+            else
+                archiveWDate = archivePath + "\\" + DateTime.Now.ToString("yyyyMMdd-hhmmss");
+            Directory.CreateDirectory(archiveWDate);
+
+            foreach (string file in files)
+            {
+                /* If file exist - delete */
+                string archived_file = archiveWDate + "\\" + Path.GetFileName(file);
+                File.Copy(file, archived_file, true);
+                File.Delete(file);
+            }
+
+            /* If there are valid files - insert to db */
+            _appDbContext.SaveChanges();
+
+            return true;
         }
     }
 }
---
+++ BusinessLogic/Data/BarcodeRepo.cs
@@ -0,0 +1,84 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Data
+{
+    public class BarcodeRepo
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BarcodeRepo()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+        public List<Barcode> GetBarcodes_ByProjectId(int projectId)
+        {
+            List<Barcode> barcodeList = _appDbContext.Barcodes
+                .Where(b => b.BarcodeFeed.ProjectId == projectId)
+                .ToList();
+            return barcodeList;
+        }
+
+        public bool DeleteBarcodes_ByBarcodeFeedId(int barcodeFeedId)
+        {
+            try
+            {
+                string sql = String.Format("DELETE FROM dbo.Barcodes where BarcodeFeedId = {0}", barcodeFeedId.ToString());
+                _appDbContext.Database.ExecuteSqlCommand(sql);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error : " + ex.Message);
+            }
+
+            return true;
+        }
+
+        public List<Barcode> BarcodeFeed_ToList(int barcodeFeedId, string stagingPath)
+        {
+            BarcodeFeed barcodeFeed = _appDbContext.BarcodeFeeds
+                .Where(bf => bf.BarcodeFeedId == barcodeFeedId && bf.IsValid)
+                .FirstOrDefault();
+
+            #region LoadPolicyFeed - Exception Handling
+            if (barcodeFeedId == 0)
+                throw new Exception(String.Format("Error : Invalid barcodeFeedId", barcodeFeedId));
+
+            if (barcodeFeed == null)
+                throw new Exception(String.Format("Error : Invalid barcodeFeedId", barcodeFeedId));
+
+            /* Get filename from db */
+            string file = Path.Combine(stagingPath, barcodeFeed.FileName);
+
+            if (!Directory.Exists(stagingPath))
+                throw new Exception(String.Format("Error : Staging {0} does not exist", stagingPath));
+
+            if (!File.Exists(file))
+                throw new Exception(String.Format("Error : File does not exist", file));
+
+            /* Check if text file */
+            if (Path.GetExtension(file).ToLower() != ".txt")
+                throw new Exception(String.Format("Error : Invalid File Extension", file));
+            #endregion
+
+            /* Check if has Policy has existing records - Delete if exist */
+            var BarcodeCheck = _appDbContext.Barcodes.Where(bc => bc.BarcodeFeedId == barcodeFeedId).FirstOrDefault();
+            if (BarcodeCheck != null)
+            {
+                this.DeleteBarcodes_ByBarcodeFeedId(barcodeFeedId);
+            }
+
+            List<Barcode> barcodes = new List<Barcode>();
+
+            string[] lines = File.ReadAllLines(file);
+            int ln = 0;
+
+            return barcodes;
+        }
+    }
